a1e26cf0ed8894785daf1a631fd6b94cc1258a37
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-11-07 18:36:41 +0100

    compose with devTools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
-import cakeReducer from './reducers/cakeReducer';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import rootReducer from './reducers/rootReducer';
 
-const store = createStore(cakeReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
