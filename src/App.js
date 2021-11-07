d4bea583a31a9907832aeb1986c575a9828dc7d7
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-11-07 22:05:38 +0100

    dependancies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+//import CakeContainer from './Components/CakeContainer';
 import './App.css';
+//import HooksCakeContainer from './Components/HooksCakeContainer';
+//import IceCreamContainer from './Components/IceCreamContainer';
+//import NewCakeContainer from './Components/NewCakeContainer';
+//import ItemContainer from './Components/ItemContainer';
+import UsersContainer from './Components/UsersContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*  <HooksCakeContainer />
+      <CakeContainer />
+      <IceCreamContainer />
+      <NewCakeContainer />
+      <ItemContainer cake />
+    <ItemContainer /> */}
+      <UsersContainer />
     </div>
   );
 }
