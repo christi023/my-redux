11d3c8b4fe82a6bb45188c7c9ed8001d07f478fc
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-11-07 22:05:06 +0100

    redux store and rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import iceCreamReducer from './iceCreamReducer';
 import cakeReducer from './cakeReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
+  user: userReducer,
 });
--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
 import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
+//import logger from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+//const middleware = [thunk];
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
